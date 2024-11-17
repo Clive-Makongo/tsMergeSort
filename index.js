a068ac02cd0c92cae3e12ed1f1663c6364d0aa4a
Clive-Makongo <clivemakongo@gmail.com>
2024-11-17 13:49:21 +0000

    completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function mergeSortRecc(array) {
+    console.log(array.length, "LENGTH OF INITIAL ARRAY");
     // stop base case
     if (array.length === 1) {
         return array;
@@ -8,28 +9,52 @@ function mergeSortRecc(array) {
     var middle = Math.floor(array.length / 2); // find middle index
     // split array at middle index
     var right = array.splice(0, middle);
+    //const left: number[] = array.slice(middle, array.length);
     console.log(right, "RIGHT");
-    console.log(array, "LEFT");
+    //console.log(left, "LEFT");
+    console.log(array, "ARRAY");
     // call function reccursively with split elements
-    mergeSortRecc(right);
-    mergeSortRecc(array);
     // feed to merge function
-    merge(right, array);
-    return array;
+    return merge(mergeSortRecc(right), mergeSortRecc(array));
 }
 ;
 // function to merge the arrays
 function merge(arrayL, arrayR) {
+    // create merged array
     var mergedArray = [];
+    // track index on left and right array
     var indexL = 0;
     var indexR = 0;
-    // compare first elements
-    //push back to earlier array reccursively
+    // compare first elements while there are more elements than index trackers
+    // console.log(arrayL, "ARRAYL");
+    // console.log(arrayR, "ARRAYR");
+    while (indexL < arrayL.length && indexR < arrayR.length) {
+        // sort based on if right first or left first is smaller
+        if (arrayL[indexL] < arrayR[indexR]) {
+            mergedArray.push(arrayL[indexL]);
+            //console.log(indexL, "INDEX L");
+            indexL++;
+            //console.log(indexL, "INDEX L++");
+            //console.log(mergedArray, "MERGED L SMALLER");
+        }
+        else {
+            mergedArray.push(arrayR[indexR]);
+            //console.log(indexR, "INDEX R");
+            indexR++;
+            //console.log(indexR, "INDEX R++");
+            //console.log(mergedArray, "MERGED R SMALLER");
+        }
+        // console.log(mergedArray, "MERGED");
+    }
     // return full array
-    return mergedArray;
+    // Concatenate the remaining elements from either `left` or `right` (if any)
+    return mergedArray
+        .concat(arrayL.slice(indexL))
+        .concat(arrayR.slice(indexR));
 }
 ;
-mergeSortRecc([38, 27, 43, 3, 9, 82, 10]);
+var final = mergeSortRecc([38, 927, 443, 35, 9, 824, 10, 334, 69, 82, 510, 1000]);
+console.log(final, "FIALLLLLLLLL");
 // PLAYGROUND TEST
 // function wrapInArray(obj: string | string[]): string[] {
 //     if (typeof obj === "string") {
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 
 function mergeSortRecc(array: number[]): number[] {
+    console.log(array.length, "LENGTH OF INITIAL ARRAY")
+
     // stop base case
     if (array.length === 1) {
         return array;
@@ -11,23 +13,20 @@ function mergeSortRecc(array: number[]): number[] {
 
     // split array at middle index
     const right: number[] = array.splice(0, middle);
-
+    //const left: number[] = array.slice(middle, array.length);
     console.log(right, "RIGHT");
-    console.log(array, "LEFT");
+    //console.log(left, "LEFT");
+    console.log(array, "ARRAY")
 
     // call function reccursively with split elements
-    mergeSortRecc(right);
-    mergeSortRecc(array);
-
     // feed to merge function
-    merge(right, array);
-    
-    return array;
+    return merge(mergeSortRecc(right), mergeSortRecc(array));
+
 };
 
 // function to merge the arrays
 function merge(arrayL: number[], arrayR: number[]): number[] {
-
+    // create merged array
     let mergedArray: number[] = [];
 
     // track index on left and right array
@@ -35,21 +34,42 @@ function merge(arrayL: number[], arrayR: number[]): number[] {
     let indexR: number = 0;
 
     // compare first elements while there are more elements than index trackers
-    while (arrayL.length > indexL && indexR < arrayR.length) {
+
+    // console.log(arrayL, "ARRAYL");
+    // console.log(arrayR, "ARRAYR");
+
+    while (indexL < arrayL.length && indexR < arrayR.length) {
         // sort based on if right first or left first is smaller
-        if ()
+        if (arrayL[indexL] < arrayR[indexR]) {
+            mergedArray.push(arrayL[indexL]);
+
+            //console.log(indexL, "INDEX L");
+            indexL++;
+            //console.log(indexL, "INDEX L++");
+            //console.log(mergedArray, "MERGED L SMALLER");
+        } else {
+            mergedArray.push(arrayR[indexR]);
+            //console.log(indexR, "INDEX R");
+            indexR++;
+            //console.log(indexR, "INDEX R++");
+            //console.log(mergedArray, "MERGED R SMALLER");
+        }
+        // console.log(mergedArray, "MERGED");
     }
 
-    //push back to earlier array reccursively
+    
 
     // return full array
 
-    return mergedArray;
+    // Concatenate the remaining elements from either `left` or `right` (if any)
+    return mergedArray
+        .concat(arrayL.slice(indexL))
+        .concat(arrayR.slice(indexR));
 };
 
-mergeSortRecc([38, 27, 43, 3, 9, 82, 10]);
+let final: number[] = mergeSortRecc([38, 927, 443, 35, 9, 824, 10, 334, 69, 82, 510, 1000]);
 
-
+console.log(final, "FIALLLLLLLLL")
 // PLAYGROUND TEST
 
 // function wrapInArray(obj: string | string[]): string[] {
